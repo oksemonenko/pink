70a2c1094a4d5f804c4b476c20c6662d5703e042
Oksana Semonenko <ok.semonenko@gmail.com>
2016-06-16 22:58:30 +0300

    :fire:  :sparkles: Правки по замечаниям наставника

--- js/main.js
+++ js/main.js
@@ -3,14 +3,18 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
+var pageHeader = document.querySelector('.page-header');
+
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('main-nav--closed')) {
         navMain.classList.remove('main-nav--closed');
         navMain.classList.add('main-nav--opened');
+        pageHeader.classList.add('page-header--nav-opened');
     } else {
         navMain.classList.add('main-nav--closed');
         navMain.classList.remove('main-nav--opened');
+        pageHeader.classList.remove('page-header--nav-opened');
     }
 });
